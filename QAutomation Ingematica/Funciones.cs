147a6329b6435f6f5cd2f2e4c02a01f86e3324e3
Gabriel <gabrielbrotzmann@live.com.ar>
2023-11-30 18:42:32 -0300

    Agregar archivos de proyecto.

---
+++ QAutomation Ingematica/Funciones.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QAutomation_Ingematica
+{
+    public class Funciones
+    {
+        public IWebElement BuscarPorXPath(string xPath, IWebDriver i)
+        {
+            try
+            {
+                var elemento = i.FindElement(By.XPath(xPath));
+                Console.WriteLine("El elemento " + elemento + "fue encontrado con exito");
+                return elemento;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("No fue posible encontrar el elemento: " + xPath);
+                return null;
+            }
+
+        }
+
+    }
+}
---
+++ QAutomation Ingematica/Test1.cs
@@ -0,0 +1,81 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V117.Debugger;
+using static System.Collections.Specialized.BitVector32;
+using System.Reflection.Emit;
+using OpenQA.Selenium.Internal;
+using System.Xml.Serialization;
+
+namespace QAutomation_Ingematica
+{
+    public class Tests
+    {
+        Funciones F = new Funciones();
+        IWebDriver driver;
+
+        [SetUp]
+        public void setup()
+        {
+            //Driver
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            Console.WriteLine("Setup");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // Busca el tercer item de la segunda pagina de Mercado Libre y chequea si esta disponible el boton de compra
+            driver.Url = "https://www.mercadolibre.com.ar";
+
+            //AceptarCookies
+            var cookie = F.BuscarPorXPath("//button[normalize-space()='Aceptar cookies']", driver);
+            cookie.Click();
+
+            //Buscador
+            var input = F.BuscarPorXPath("//input[@id='cb1-edit']", driver);
+            input.SendKeys("Smartphone");
+
+            //ClickeaBuscador
+            var searchBtn = F.BuscarPorXPath("//div[@aria-label='Buscar']", driver);
+            searchBtn.Click();
+
+            //ClickeaSiguientePagina
+            var sigPag = F.BuscarPorXPath("//span[@class='andes-pagination__arrow-title']", driver);
+            sigPag.Click();
+            
+            //ClickTercerItem
+            var item = F.BuscarPorXPath("/html[1]/body[1]/main[1]/div[1]/div[2]/section[1]/ol[1]/li[3]/div[*]/div[1]/div[2]/div[*]/a[1]/h2[1]", driver);
+            item.Click();
+
+            /*BuscarBotonCompra (Busca el boton de compra, si esta disponible, entonces tambien el producto, si hay distintas
+            opciones de compra, clickea y busca el boton nuevamente */
+            try
+            {
+                var disponibilidad = F.BuscarPorXPath("//span[normalize-space()='Comprar ahora']", driver);
+                if (disponibilidad != null)
+                {
+                    Console.WriteLine("El producto se encuentra disponible");
+                }else if (disponibilidad is null)
+                {
+                    var opcionesDeCompra = F.BuscarPorXPath("//span[normalize-space()='Ver opciones de compra']", driver);
+                    opcionesDeCompra.Click();
+                    disponibilidad = F.BuscarPorXPath("//span[normalize-space()='Comprar ahora']", driver);
+                    if (disponibilidad != null)
+                    {
+                        Console.WriteLine("El producto se encuentra disponible");
+                    }else { Console.WriteLine("El producto no se encuentra disponible"); } 
+                }
+            }
+            catch (Exception e) { Console.WriteLine(e); }
+        }
+
+        [TearDown]
+        public void tearDown()
+        {
+            driver.Close();
+            driver.Quit();
+            Console.WriteLine("El test ha finalizado");
+        }
+    }
+}
